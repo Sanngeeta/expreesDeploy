0b00d6c6239cdead636dfddeff380e1050364a0f
Sanngeeta <sangeetap20@navgurkul.org>
2022-11-10 16:48:50 +0530

    some changes profile

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -16,22 +16,6 @@ npm install
 # pm2 start npm --name "express-app" 
 
 
-# Start all applications
-pm2 start ecosystem.config.js
-
-# Stop all
-pm2 stop ecosystem.config.js
-
-# Restart all
-pm2 restart ecosystem.config.js
-
-# Reload all
-pm2 reload ecosystem.config.js
-
-# Delete all
-pm2 delete ecosystem.config.js
-
-
 
 #start our node app in the background
 node app.js > app.out.log 2> app.err.log < /dev/null & 
