e5c333ab3362ad162878e282e616252a86254735
Sanngeeta <sangeetap20@navgurkul.org>
2022-11-10 16:52:28 +0530

    some changes profile

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -15,7 +15,20 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 # pm2 start npm --name "express-app" 
 
+# Start all applications
+pm2 start ecosystem.config.js
 
+# Stop all
+pm2 stop ecosystem.config.js
+
+# Restart all
+pm2 restart ecosystem.config.js
+
+# Reload all
+pm2 reload ecosystem.config.js
+
+# Delete all
+pm2 delete ecosystem.config.js
 
 #start our node app in the background
 node app.js > app.out.log 2> app.err.log < /dev/null & 
