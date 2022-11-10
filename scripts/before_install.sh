f263225e27993942b6e49304b9ff34dbff522117
Sanngeeta <sangeetap20@navgurkul.org>
2022-11-10 12:11:12 +0530

    Some changes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# download node and npm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+# # download node and npm
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install node
+
+
+# add nodejs to yum
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
+yum install nodejs -y #default-jre ImageMagick
+
+# install pm2 module globaly
+npm install -g pm2
+pm2 update
+
+
+
 
 
 #create our working directory if it doesnt exist.
